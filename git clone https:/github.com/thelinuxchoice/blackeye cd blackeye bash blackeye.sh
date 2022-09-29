a536e10280f07091147718485c73f929f94ead39
slDILAdila <114671990+slDILAdila@users.noreply.github.com>
2022-09-29 18:58:16 +0530

    Create blackeye cd blackeye bash blackeye.sh

---
+++ git clone https:/github.com/thelinuxchoice/blackeye cd blackeye bash blackeye.sh
@@ -0,0 +1,14 @@
+- 👋 Hi, I’m @slDILAdila
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+slDILAdila/slDILAdila is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+bash blackeye.sh
+
